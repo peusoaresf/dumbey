12d0c081257f328a8931a0ffd382212ce9a05a35
Pedro Ferraz <psoaresferraz@gmail.com>
2026-09-20 00:27:33 +0100

    feat: make messages list layout usable

--- src/main/java/gui/ChatContainer.java
+++ src/main/java/gui/ChatContainer.java
@@ -1,40 +1,105 @@
 package gui;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JList;
-import javax.swing.JScrollPane;
+import java.awt.*;
+
+import javax.swing.*;
 
 public class ChatContainer extends JScrollPane {
-    private final DefaultListModel<String> messages = new DefaultListModel<>() {{
-        addElement("Agent: Hello, I'm an AI agent. How can I assist you?");
-    }};
+    private final MessagesPanel messages = new MessagesPanel();
 
     public ChatContainer() {
         super();
 
-        this.setViewportView(new JList<>(messages));
+        this.setViewportView(messages);
     }
 
     public void addError(String error) {
-        addMessage("Error", error);
+        addMessage(new ErrorMessage(error));
     }
 
     public void addAgentReply(String reply) {
-        addMessage("Agent", reply);
+        addMessage(new AgentMessage(reply));
     }
 
     public void addUserPrompt(String prompt) {
-        addMessage("You", prompt);
+        addMessage(new UserMessage(prompt));
     }
 
-    private void addMessage(String actor, String message) {
-        messages.addElement(" ");
-        messages.addElement(actor + ": " + message);
+    private void addMessage(MessageText message) {
+        messages.add(message);
         scrollToBottom();
     }
 
     private void scrollToBottom() {
         this.validate();
-        this.getVerticalScrollBar().setValue(this.getVerticalScrollBar().getMaximum());
+        SwingUtilities.invokeLater(() -> {
+            this.getVerticalScrollBar().setValue(this.getVerticalScrollBar().getMaximum());
+        });
+    }
+
+    private static class MessageText extends JTextArea {
+        public MessageText(String message) {
+            super(message);
+            this.setEditable(false);
+            this.setWrapStyleWord(true);
+            this.setLineWrap(true);
+            this.setBorder(BorderFactory.createEmptyBorder(0, 0, 32, 0));
+        }
+    }
+
+    private static class AgentMessage extends MessageText {
+        public AgentMessage(String message) {
+            super(message);
+            this.setForeground(Color.BLACK);
+        }
+    }
+
+    private static class ErrorMessage extends MessageText {
+        public ErrorMessage(String message) {
+            super(message);
+            this.setForeground(Color.RED);
+        }
+    }
+
+    private static class UserMessage extends MessageText {
+        public UserMessage(String message) {
+            super(message);
+            this.setForeground(Color.GRAY);
+            this.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }
+    }
+
+    private static class MessagesPanel extends JPanel implements Scrollable {
+        public MessagesPanel() {
+            setBackground(Color.WHITE);
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+            add(new MessageText("Hello, I'm an AI agent. How can I assist you?"));
+        }
+
+        @Override
+        public Dimension getPreferredScrollableViewportSize() {
+            return getPreferredSize();
+        }
+
+        @Override
+        public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+            return 16;
+        }
+
+        @Override
+        public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+            return visibleRect.height;
+        }
+
+        @Override
+        public boolean getScrollableTracksViewportWidth() {
+            return true;
+        }
+
+        @Override
+        public boolean getScrollableTracksViewportHeight() {
+            return false;
+        }
     }
 }
